327129590aa69dc44b4bcb61fe800a19e21fce2d
linyanheng <Powerworker1234@gmail.com>
2018-03-05 23:35:31 +0800

    分數比較

--- src/code_1221/ScoreSort.java
+++ src/code_1221/ScoreSort.java
@@ -10,16 +10,21 @@ public class ScoreSort {
 
 
     public static void main(String[] args) {
+         //宣告一個ScoreSort 的物件陣列，其大小為４
          ScoreSort[] student = new ScoreSort[4];
+         //第１～４筆資料藉由建構函數給初值
          student[0] = new ScoreSort("S001",87);
          student[1] = new ScoreSort("S002",90);
          student[2] = new ScoreSort("S003",52);
          student[3] = new ScoreSort("S003",95);
+         //宣告一個LinkedList<ＳcoreSore> 的資料型態
          List<ScoreSort> students = new LinkedList<>();
+         //將
          students.add(student[0]);
          students.add(student[1]);
          students.add(student[2]);
          students.add(student[3]);
+         //呼叫list的sort 方法，並實作Ｃomparator方法
          students.sort(new Comparator<ScoreSort>() {
              @Override
              public int compare(ScoreSort o1, ScoreSort o2) {
